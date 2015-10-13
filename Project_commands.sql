4d1bd016d92a0ec3eb3bc28a88319764b3f0aad1
John Grant <john.grant493@gmail.com>
2015-10-13 10:41:41 -0400

    All tables can be placed into mysql table except connected_to. Connected_To needs a primary key that isn't a foreign key. More work needs to be done.

--- Project_commands.sql
+++ Project_commands.sql
@@ -12,34 +12,34 @@ CREATE TABLE Usr (
 CREATE TABLE User_Transaction (
     utdate date,
     tid char(30) NOT NULL,
-    PRIMARY KEY (tid)
-    FOREIGN KEY (busername) REFERENCES Usr (username)
-            ON DELETE NO ACTION,
-    FOREIGN KEY (susername) REFERENCES Usr (username)
-            ON DELETE NO ACTION,
-    FOREIGN KEY (iid) REFERENCES Item
-            ON DELETE NO ACTION
+    cusername char(20) NOT NULL,
+    busername char(20) NOT NULL,
+    PRIMARY KEY (tid, cusername, busername),
+    FOREIGN KEY (cusername) REFERENCES Usr(username),
+    FOREIGN Key (busername) REFERENCES Usr(username)
 )ENGINE=INNODB;
 
 CREATE TABLE Item (
     iid char(20) NOT NULL,
-    descr char(350),
+    descr char(250),
     location char(20) NOT NULL,
     PRIMARY KEY (iid)
 )ENGINE=INNODB;
 
 CREATE TABLE Auction_Item (
+    iid char(20) NOT NULL,
     bid char(20) NOT NULL,
     bidder char(20) NOT NULL,
     min_price REAL,
     PRIMARY KEY (iid),
-    FOREIGN KEY (iid) REFERENCES Item.iid
+    FOREIGN KEY (iid) REFERENCES Item(iid)
 )ENGINE=INNODB;
 
 CREATE TABLE Sale_Item (
+    iid char(20) NOT NULL,
     price REAL,
     PRIMARY KEY (iid),
-    FOREIGN KEY (iid) REFERENCES Item.iid
+    FOREIGN KEY (iid) REFERENCES Item(iid)
 )ENGINE=INNODB;
 
 CREATE TABLE Category (
@@ -49,21 +49,24 @@ CREATE TABLE Category (
 )ENGINE=INNODB;
 
 CREATE TABLE Categorized (
-    PRIMARY KEY (iid) REFERENCES Item (iid),
-    FOREIGN KEY (pcid) REFERENCES Category (cid)
-            ON DELETE NO ACTION
-            ON UPDATE CASCADE,
-    FOREIGN KEY (ccid) REFERENCES Category (cid)
-            ON DELETE NO ACTION
-            ON UPDATE CASCADE
+    iid char(20) NOT NULL,
+    ccid char(30) NOT NULL,
+    pcid char(30) NOT NULL,
+    PRIMARY KEY (iid),
+    FOREIGN KEY (iid) REFERENCES Item(iid),
+    FOREIGN Key (ccid) REFERENCES Category(cid),
+    FOREIGN Key (pcid) REFERENCES Category(cid)
 )ENGINE=INNODB;
 
 CREATE TABLE Connected_To (
-    FOREIGN KEY (mcid) REFERENCES Category (iid)
-    FOREIGN KEY (cid) REFERENCES Category
+    cid char(30) NOT NULL,
+    mcid char(30) NOT NULL,
+    FOREIGN KEY (mcid) REFERENCES Category (iid),
+    FOREIGN KEY (cid) REFERENCES Category(cid)
 )ENGINE=INNODB;
 
 CREATE TABLE Loc_Addresses (
+    username char(20) NOT NULL,
     aid char(30) NOT NULL,
     street char(25) NOT NULL,
     city char(20) NOT NULL,
@@ -71,15 +74,16 @@ CREATE TABLE Loc_Addresses (
     zip REAL,
     apt_number REAL,
     PRIMARY KEY (aid, username),
-    FOREIGN KEY (username) REFERENCES Usr
+    FOREIGN KEY (username) REFERENCES Usr(username)
             ON DELETE CASCADE
 )ENGINE=INNODB;
 
 CREATE TABLE Cont_Phone (
+    username char(20) NOT NULL,
     number REAL,
     phid char(20) NOT NULL,
     PRIMARY KEY (phid, username),
-    FOREIGN KEY (username) REFERENCES Usr
+    FOREIGN KEY (username) REFERENCES Usr(username)
             ON DELETE CASCADE
 )ENGINE=INNODB;
 
@@ -87,45 +91,86 @@ CREATE TABLE Cash_Credit_Card (
     cnumber REAL,
     cname char(20) NOT NULL,
     ctype char(20) NOT NULL,
+    username char(20) NOT NULL,
     expiration date,
     PRIMARY KEY (cnumber, username),
-    FOREIGN KEY (username) REFERENCES Usr
+    FOREIGN KEY (username) REFERENCES Usr(username)
             ON DELETE CASCADE
 )ENGINE=INNODB;
 
 CREATE TABLE Search_Key (
     word char(20),
     kid char(30) NOT NULL,
-    PRIMARY KEY (kid, iid)
-    FOREIGN KEY (iid) REFERENCES Item
+    iid char(20) NOT NULL,
+    PRIMARY KEY (kid, iid),
+    FOREIGN KEY (iid) REFERENCES Item(iid)
             ON DELETE CASCADE
 )ENGINE=INNODB;
 
 CREATE TABLE Supplier_Trans (
+    iid char(20) NOT NULL,
+    username char(20) NOT NULL,
     stname char(20) NOT NULL,
     poc char(20) NOT NULL,
     stid char(30) NOT NULL,
     staddress char(30) NOT NULL,
     stphone REAL,
     PRIMARY KEY (stid, iid, username),
-    FOREIGN KEY (iid) REFERENCES Item
+    FOREIGN KEY (iid) REFERENCES Item(iid)
             ON DELETE NO ACTION,
-    FOREIGN KEY (username) REFERENCES Usr
+    FOREIGN KEY (username) REFERENCES Usr(username)
             ON DELETE NO ACTION
 )ENGINE=INNODB;
 
 CREATE TABLE Runs_Shop(
+    username char(20) NOT NULL,
     rsname char(20) NOT NULL,
     shid char(30) NOT NULL,
     PRIMARY KEY (shid, username),
-    FOREIGN KEY (username) REFERENCES Usr
+    FOREIGN KEY (username) REFERENCES Usr(username)
             ON DELETE CASCADE  
 )ENGINE=INNODB;
 
 CREATE TABLE Stocked_By (
+    iid char(20) NOT NULL,
+    shid char(30) NOT NULL,
     PRIMARY KEY (shid, iid),
-    FOREIGN KEY (iid) REFERENCES Item
+    FOREIGN KEY (iid) REFERENCES Item(iid)
             ON DELETE NO ACTION,
-    FOREIGN KEY (shid) REFERENCES Runs_Shop
+    FOREIGN KEY (shid) REFERENCES Runs_Shop(shid)
             ON DELETE CASCADE
 ) ENGINE=INNODB;
+
+CREATE TABLE Whishes_For (
+    wid char(30) NOT NULL,
+    username char(20) NOT NULL,
+    PRIMARY KEY (wid, username),
+    FOREIGN KEY (username) REFERENCES Usr(username)
+            ON DELETE CASCADE
+            ON UPDATE CASCADE
+) ENGINE=INNODB;
+
+CREATE TABLE Filled_With (
+    ranking REAL,
+    wid char(30) NOT NULL,
+    iid char(20) NOT NULL,
+    PRIMARY KEY (wid, iid),
+    FOREIGN KEY (iid) REFERENCES Item(iid),
+    FOREIGN KEY (wid) REFERENCES Whishes_For(wid) 
+) ENGINE=INNODB;
+
+CREATE TABLE Supplier_Stocked (
+    iid char(20) NOT NULL,
+    username char(20) NOT NULL,
+    PRIMARY KEY (iid, username),
+    FOREIGN KEY (iid) REFERENCES Item(iid),
+    FOREIGN KEY (username) REFERENCES Usr (username)
+)ENGINE=INNODB;
+
+CREATE TABLE User_Stocked (
+    iid char(20) NOT NULL,
+    username char(20) NOT NULL,
+    PRIMARY KEY (iid, username),
+    FOREIGN KEY (iid) REFERENCES Item(iid),
+    FOREIGN KEY (username) REFERENCES Usr (username)
+)ENGINE=INNODB;
