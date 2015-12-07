dabd55f1bd5f4fe29c2584945d663eecac2c073b
Nicholas Denaro <NicholasCDenaro@aol.com>
2015-12-07 15:19:36 -0500

    Aggregate transaction reports.

--- src/backend/api/GeneralDickWith.php
+++ src/backend/api/GeneralDickWith.php
@@ -2,7 +2,11 @@
 
 require_once("ApiLibrary.php");
 
-echo json_encode(getParentCategory("432g7h6g45435ff"));
+//echo json_encode(getParentCategory("432g7h6g45435ff"));
 
+//$headers = "From: SteamBackwards@gmail.com\r\nReply-To: SteamBackwards@gmail.com\r\nX-Mailer: PHP/".phpversion();
+
+//set_time_limit(1000);
+echo mail("SteamBackwards@gmail.com","subject","message");
 
 ?>
